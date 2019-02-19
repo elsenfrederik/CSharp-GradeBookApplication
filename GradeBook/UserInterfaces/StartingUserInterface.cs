d999d9553f0711e59600d708dd447184ead66b59
Frederik.Elsen <frederik.elsen@gfk.com>
2019-02-19 11:54:14 +0100

    Completed the module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -1,5 +1,6 @@
 ﻿using GradeBook.GradeBooks;
 using System;
+using GradeBook.Enums;
 
 namespace GradeBook.UserInterfaces
 {
@@ -34,13 +35,29 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2].ToLower();
+
+            BaseGradeBook gradeBook;
+            switch (type)
+            {
+                case "standaard":
+                    gradeBook = new StandardGradeBook(name);
+                    break;
+                case "ranked":
+                    gradeBook = new RankedGradeBook(name);
+                    break;
+                default:
+                    Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
+                    return;
+            }
+
+            
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
